bd4f0334826d83ffe99907df95da409fc34dfb0e
Ted Scharff <e.b.scharff@nasa.gov>
2012-03-07 12:50:41 -0800

    Kind of broken implementation using multiproccessing.pool

--- deploy.py
+++ deploy.py
@@ -4,12 +4,17 @@ import httplib
 import hashlib
 import os
 import sys
+import time
 import subprocess
-import optparse
+import argparse
 import sqlite3
+from multiprocessing import Pool, Queue
+import multiprocessing, logging
+
+logger = multiprocessing.log_to_stderr()
+logger.setLevel(multiprocessing.SUBDEBUG)
 
 DB_TRANSACTION_SIZE = 100000 # for bulk commits and updates to the inventory database
-
 global gsutil_path
 
 def gsutil(*args):
@@ -24,35 +29,80 @@ def md5_digest(filepath):
             md5.update(bytes)
     return md5.hexdigest()
 
-def sync_file(path, bucket, start_path):
-    print path + "...",
-    sys.stdout.flush()
+def sync_file(path, bucket, start_path, check_remote=True):
+    logger.info( "SYNC " + path + "...")
+    if start_path not in path:
+        path = os.path.join(start_path, path)
     relpath = os.path.relpath(path, start_path)
     url = "http://commondatastorage.googleapis.com/%s/%s" % (bucket, relpath)
-    fail = False
-    conn = httplib.HTTPConnection(bucket+'.commondatastorage.googleapis.com')
-    conn.request('HEAD', '/'+relpath)
-    response = conn.getresponse() 
-    if response.status == 200:
-        remote_hash = response.getheader('etag').replace('"','')
-        local_hash = md5_digest(path)
-        if remote_hash == local_hash:
-            print "OK"
-            return
-        else:
-            print "MD5 mismatch... %s :: %s" % (local_hash, remote_hash)
-    print "UPLOADING...",
+    if check_remote:
+        conn = httplib.HTTPConnection(bucket+'.commondatastorage.googleapis.com')
+        conn.request('HEAD', '/'+relpath)
+        response = conn.getresponse() 
+        if response.status == 200:
+            remote_hash = response.getheader('etag').replace('"','')
+            local_hash = md5_digest(path)
+            if remote_hash == local_hash:
+                print "OK"
+                return
+            else:
+                raise Exception( "MD5 mismatch... %s :: %s" % (local_hash, remote_hash) )
+    logger.debug("UPLOADING " + path)
     gsutil('cp', path, "gs://"+bucket+"/"+relpath)
-    print "DONE!"
+    logger.debug("UPLOADED " +path)
 
-def sync_recursive(source_path, dest_bucket):
+def sync_recursive(source_dir, dest_bucket):
     """ Sync files from source path to dest_bucket on GCS."""
     gsutil( "setdefacl", "public-read", "gs://"+dest_bucket) # make uploads world-readable
-    print "%s --> gs://%s" % (source_path, dest_bucket)
-    for (dirpath, dirnames, filenames) in os.walk(source_path):
+    print "%s --> gs://%s" % (source_dir, dest_bucket)
+    for (dirpath, dirnames, filenames) in os.walk(source_dir):
         print len(filenames)
         for filename in filenames:
-            sync_file(os.path.join(dirpath, filename), dest_bucket, source_path)
+            sync_file(os.path.join(dirpath, filename), dest_bucket, source_dir)
+
+def work(path, dest_bucket, start_path):
+    sync_file(path, dest_bucket, start_path, check_remote=False)
+
+def mark_complete(filepath):
+    connection = sqlite3.Connection(options.inventory_db)
+    cursor = connection.cursor()
+    logger.debug("UPDATING: %s" % filepath)
+    cursor.execute('UPDATE files set remote_exists = 1 where path = ?', (filepath,))
+    logger.debug("UPDATED: %d" % cursor.rowcount)
+    assert cursor.rowcount == 1
+    connection.commit()
+    logger.debug("COMMITTED")
+
+def sync_parallel(filename_iterator, options):
+
+    response = None
+    while response not in ('y','n'):
+        response = raw_input("Do you want to run a remote inventory first?  You probably do. (y/n)")
+
+    #work(filename_iterator.next(), options.dest_bucket, options.source_dir) #TMP
+    pool = Pool(2, initargs=(options.inventory_db,))
+    results = set()
+    for filename in filename_iterator:
+        logger.debug("Adding " + filename)
+        results.add(pool.apply_async(work, (filename, options.dest_bucket, options.source_dir), callback=lambda result, path=filename, results=results: mark_complete(path)))
+    logger.debug("initial results: %d" % len(results))
+    pool.close()
+    pool.join()
+
+def names_from_db(dbname, which="untransfered"):
+    global connection
+    qry = "SELECT path FROM files"
+    if which != "all":
+        qry += " WHERE remote_exists IS NULL"
+    print qry
+    cursor = connection.cursor()
+    cursor.execute(qry)
+    records = cursor.fetchmany()
+    while records:
+        for record in records:
+            yield record[0]
+        records = cursor.fetchmany()
+        
 
 def search_for_gsutil():
     # Search for gsutil executable in the $PATH
@@ -77,11 +127,12 @@ def ensure_exists(path):
         pass
 
 def local_inventory(source_dir, bucketname):
-    inventory_path = "."+bucketname
+    inventory_path = options.inventory_path
     ensure_exists(inventory_path)
-    conn = sqlite3.connect(os.path.join(inventory_path, "inventory.db"))
+    conn = sqlite3.connect(options.inventory_db)
     cursor = conn.cursor()
     cursor.execute("DROP TABLE IF EXISTS files;")
+    cursor.execute("DROP INDEX IF EXISTS idx_path;")
     cursor.execute("""
     CREATE TABLE files (
         id INTEGER PRIMARY KEY,
@@ -91,13 +142,16 @@ def local_inventory(source_dir, bucketname):
         remote_exists INTEGER
     );
     """)
+    conn.commit()
 
     count = 0
     for (dirpath, dirnames, filenames) in os.walk(source_dir):
         for filename in filenames:
             filepath = os.path.join(dirpath, filename)
-            filehash = md5_digest(filepath)
-            cursor.execute('INSERT INTO files (path, local_hash) VALUES (?, ?);',  (filepath, filehash))
+            relpath = os.path.relpath(filepath, source_dir)
+            #filehash = md5_digest(filepath)
+            #cursor.execute('INSERT INTO files (path, local_hash) VALUES (?, ?);',  (relpath, filehash))
+            cursor.execute('INSERT INTO files (path) VALUES (?);',  (relpath, ))
             count += 1
             if count % DB_TRANSACTION_SIZE == 0:
                 conn.commit()
@@ -106,51 +160,77 @@ def local_inventory(source_dir, bucketname):
         conn.commit()
     print "Creating index...",
     cursor.execute('CREATE INDEX IF NOT EXISTS idx_path ON files(path);')
+    cursor.execute('CREATE INDEX IF NOT EXISTS idx_remote_exist ON files(remote_exists);')
     print "Done."
 
-def remote_inventory(source_dir, bucketname):
-    inventory_path = "."+bucketname
-    assert os.path.exists(os.path.join(inventory_path, 'inventory.db'))
+def remote_inventory(source_dir, bucketname, options):
+    inventory_path = options.inventory_path 
+    assert os.path.exists(options.inventory_db)
 
-    print "Fetching remote listing...",
     listfile_name = os.path.join(inventory_path, 'remote_list.txt')
-    with open(listfile_name, 'w') as listfile:
-        listfile.write(gsutil('ls', 'gs://"+bucketname'))
-    print "done."
+    if not options.no_refresh:
+        print "Fetching remote listing...",
+        with open(listfile_name, 'w') as listfile:
+            #listfile.write(gsutil('ls', 'gs://'+bucketname))
+            p = subprocess.Popen((gsutil_path, 'ls', 'gs://'+bucketname), stdout=subprocess.PIPE)
+            for line in p.stdout:
+                listfile.write(line)
+            assert p.wait() == 0
+        print "done."
 
-    conn = sqlite3.connect(os.path.join(inventory_path, "inventory.db"))
+    conn = sqlite3.connect(options.inventory_db)
     cursor = conn.cursor()
     cursor.execute('CREATE INDEX IF NOT EXISTS idx_path ON files(path);')
 
     with open(listfile_name, 'r') as listfile:
         i = 0
         for line in listfile:
-            path = line.strip()[len("gs://ge-mars/")-1:]
-            cursor.execute('UPDATE files SET remote_exists = 1 WHERE path == ?', path)
+            path = line.strip()[len("gs://ge-mars/"):]
+            cursor.execute('UPDATE files SET remote_exists = 1 WHERE path == ?', (path,))
             i += 1
             if i % DB_TRANSACTION_SIZE == 0:    
                 conn.commit()
+                print "%d updates" % i
         else:
             conn.commit()
     
 
 if __name__ == "__main__":
-    parser = optparse.OptionParser()
-    parser.add_option('--dir', dest="source_dir")
-    parser.add_option('--bucket', dest="dest_bucket")
-    parser.add_option('--gsutil-path', dest='gsutil_path')
-    parser.add_option('--local-inventory', action='store_true', dest='local_inventory')
+    #parser = optparse.OptionParser()
+#    parser.add_option('--dir', dest="source_dir")
+#    parser.add_option('--bucket', dest="dest_bucket")
+#    parser.add_option('--gsutil-path', dest='gsutil_path')
+#    parser.add_option('--local-inventory', action='store_true', dest='local_inventory')
 
-    parser.set_defaults(source_dir=settings.OUTPUT_PATH_BASE, dest_bucket=settings.TARGET_GCS_BUCKET, gsutil_path=None)
-    options, args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', choices=['local-inventory', 'remote-inventory', 'sync-serial', 'sync-parallel'])
+    parser.add_argument('--dir', dest='source_dir', metavar='rood source directory', default=settings.OUTPUT_PATH_BASE)
+    parser.add_argument('--bucket', dest='dest_bucket', default=settings.TARGET_GCS_BUCKET)
+    parser.add_argument('--gsutil-path', dest='gsutil_path')
+    parser.add_argument('--no-refresh', action='store_true', default=False, help="For remote inventory, don't refresh the GCS listing, just do the db updates.")
 
+    options = parser.parse_args()
+    print "%s --> %s" % (options.source_dir, options.dest_bucket)
 
+    # derived options
+    options.inventory_path = '.'+options.dest_bucket
+    options.inventory_db = os.path.join(options.inventory_path, 'inventory.db')
+
+    connection = sqlite3.Connection(options.inventory_db)
+
+    global gsutil_path
     if options.gsutil_path:
         gsutil_path = options.gsutil_path
     else:
         gsutil_path = search_for_gsutil()
 
-    if options.local_inventory:
+    if options.command == 'local-inventory':
         local_inventory(options.source_dir, options.dest_bucket)
-    else:
+    elif options.command == 'remote-inventory':
+        remote_inventory(options.source_dir, options.dest_bucket, options)
+    elif options.command == 'sync-serial':
         sync_recursive(options.source_dir, options.dest_bucket)
+    elif options.command == 'sync-parallel':
+        sync_parallel(names_from_db(options.inventory_db), options)
+    else:
+        assert False
