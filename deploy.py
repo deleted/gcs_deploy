c025886b875fa35531f4bb9f2aa87f15206c3832
Ted Scharff <e.b.scharff@nasa.gov>
2012-03-08 10:02:13 -0800

    deploy.py: all database access moved to a single process. now using queues instead of pool for multiprocessing

--- deploy.py
+++ deploy.py
@@ -8,8 +8,8 @@ import time
 import subprocess
 import argparse
 import sqlite3
-from multiprocessing import Pool, Queue
 import multiprocessing, logging
+import Queue
 
 logger = multiprocessing.log_to_stderr()
 logger.setLevel(multiprocessing.SUBDEBUG)
@@ -60,11 +60,8 @@ def sync_recursive(source_dir, dest_bucket):
         for filename in filenames:
             sync_file(os.path.join(dirpath, filename), dest_bucket, source_dir)
 
-def work(path, dest_bucket, start_path):
-    sync_file(path, dest_bucket, start_path, check_remote=False)
-
-def mark_complete(filepath):
-    connection = sqlite3.Connection(options.inventory_db)
+def mark_complete(filepath, connection, dblock=None):
+    if dblock: dblock.acquire()
     cursor = connection.cursor()
     logger.debug("UPDATING: %s" % filepath)
     cursor.execute('UPDATE files set remote_exists = 1 where path = ?', (filepath,))
@@ -72,36 +69,72 @@ def mark_complete(filepath):
     assert cursor.rowcount == 1
     connection.commit()
     logger.debug("COMMITTED")
+    time.sleep(0.2)
+    if dblock: dblock.release()
+
+def worker(task_q, result_q, finished, dbname):
+    while True:
+        if finished.is_set():
+            break
+        try:
+            path, dest_bucket, start_path = task_q.get(False, 0.1) # retry after a timeout
+        except Queue.Empty:
+            continue
+        sync_file(path, dest_bucket, start_path, check_remote=False)
+        result_q.put(path)
+        task_q.task_done()
+    
+def db_reporter(q, finished, dbname):
+    connection = sqlite3.Connection(dbname)
+    while True:
+        if finished.is_set():
+            break
+        try:
+            path = q.get(False, 0.1) # retry after a timeout
+        except Queue.Empty:
+            continue
+        mark_complete(path, connection)
+        q.task_done()
+
 
 def sync_parallel(filename_iterator, options):
-
     response = None
     while response not in ('y','n'):
         response = raw_input("Do you want to run a remote inventory first?  You probably do. (y/n)")
 
-    #work(filename_iterator.next(), options.dest_bucket, options.source_dir) #TMP
-    pool = Pool(2, initargs=(options.inventory_db,))
-    results = set()
-    for filename in filename_iterator:
-        logger.debug("Adding " + filename)
-        results.add(pool.apply_async(work, (filename, options.dest_bucket, options.source_dir), callback=lambda result, path=filename, results=results: mark_complete(path)))
-    logger.debug("initial results: %d" % len(results))
-    pool.close()
-    pool.join()
+    task_q = multiprocessing.JoinableQueue(options.max_queue_size)
+    result_q = multiprocessing.JoinableQueue(options.max_queue_size)
+    finished = multiprocessing.Event()
+    subprocesses = [ multiprocessing.Process(target=worker, args=(task_q, result_q, finished, options.inventory_db)) for i in range(options.num_processes) ]
+    reporter = multiprocessing.Process(target=db_reporter, args=(result_q, finished, options.inventory_db) )
+    subprocesses.append(reporter)
+    for p in subprocesses:
+        p.start()
+    for path in filename_iterator:
+        task_q.put( (path, options.dest_bucket, options.source_dir) )
+    task_q.join()
+    result_q.join()
+    finished.set()
+    logger.info("Finished.  Waiting for subprocesses to join")
+    for p in subprocesses:
+        p.join()
 
-def names_from_db(dbname, which="untransfered"):
+def names_from_db(dbname, which="untransfered", dblock=None):
     global connection
     qry = "SELECT path FROM files"
     if which != "all":
         qry += " WHERE remote_exists IS NULL"
+    qry += " LIMIT %d" % options.db_chunk_size
     print qry
-    cursor = connection.cursor()
-    cursor.execute(qry)
-    records = cursor.fetchmany()
-    while records:
+    while True:
+        cursor = connection.cursor()
+        if dblock: dblock.acquire()
+        cursor.execute(qry)
+        if dblock: dblock.release()
+        records = cursor.fetchall()
+        if len(records) == 0: break
         for record in records:
             yield record[0]
-        records = cursor.fetchmany()
         
 
 def search_for_gsutil():
@@ -208,6 +241,9 @@ if __name__ == "__main__":
     parser.add_argument('--bucket', dest='dest_bucket', default=settings.TARGET_GCS_BUCKET)
     parser.add_argument('--gsutil-path', dest='gsutil_path')
     parser.add_argument('--no-refresh', action='store_true', default=False, help="For remote inventory, don't refresh the GCS listing, just do the db updates.")
+    parser.add_argument('-p', type=int, dest='num_processes', help='Number of subprocesses to spawn for sync-parallel', default=8)
+    parser.add_argument('--max_queue_size', type=int, help="Size of the task queue", default=200)
+    parser.add_argument('--db_chunk_size', type=int, help="Number of records to pull from the database per read", default=200)
 
     options = parser.parse_args()
     print "%s --> %s" % (options.source_dir, options.dest_bucket)
